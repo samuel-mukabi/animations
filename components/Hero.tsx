f2c1dce3d31ba08aa13cc4dfe266485055925e47
Flouric <95908184+platoni-c@users.noreply.github.com>
2026-02-17 20:45:12 +0300

    Implement hero scroll animation

---
+++ components/Hero.tsx
@@ -0,0 +1,114 @@
+'use client'
+
+import React, {useRef} from 'react'
+import {useGSAP} from "@gsap/react";
+import gsap from "gsap";
+import { SplitText } from "gsap/SplitText";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
+const Hero = () => {
+    const videoRef = useRef<HTMLVideoElement>(null)
+
+
+    useGSAP(() => {
+        const isMobile = window.matchMedia("(max-width: 768px)").matches;
+        const heroSplit = new SplitText('.title', {type: 'chars, words'})
+        const paragraphSplit = new SplitText('.subtitle', {type: 'lines'})
+
+        heroSplit.chars.forEach((char) => char.classList.add('text-gradient'))
+
+        gsap.from(heroSplit.chars, {
+            yPercent: 100,
+            duration: 1,
+            ease: 'expo.out',
+            stagger: 0.06,
+        })
+
+        gsap.from(paragraphSplit.lines, {
+            opacity: 0,
+            yPercent: 100,
+            duration: 1.8,
+            ease: 'expo.out',
+            stagger: 0.06,
+            delay: 1
+        })
+
+        gsap.timeline({
+            scrollTrigger: {
+                trigger: '#hero',
+                start: 'top top',
+                end: 'bottom top',
+                scrub: true,
+                pin: true,
+                pinSpacing: true
+            }
+        })
+            .to('.right-leaf', { y: 200}, 0)
+            .to('.left-leaf', { y: -200}, 0)
+
+        const startValue = isMobile ? 'top 50%' : 'center 60%'
+        const endValue = isMobile ? '120% top' : 'bottom top'
+
+        const video = videoRef.current;
+        if (video) {
+            const onVideoLoad = () => {
+                gsap.to(video, {
+                    scrollTrigger: {
+                        trigger: 'video',
+                        start: startValue,
+                        end: endValue,
+                        scrub: true,
+                        pin: true,
+                    },
+                    currentTime: video.duration || 0,
+                    ease: 'none',
+                    onUpdate: () => {
+                        // Ensure the video doesn't play on its own
+                        if (!video.paused) video.pause();
+                    }
+                })
+            };
+
+            if (video.readyState >= 2) {
+                onVideoLoad();
+            } else {
+                video.addEventListener('loadedmetadata', onVideoLoad);
+            }
+        }
+    }, [])
+
+    return (
+        <>
+            <section id='hero' className='noisy'>
+                <h1 className="title">MOJITO</h1>
+
+                <img src="/images/hero-left-leaf.png" alt="left-leaf" className='left-leaf'/>
+                <img src="/images/hero-right-leaf.png" alt="right-leaf" className='right-leaf'/>
+
+                <div className="body">
+                    <div className="content">
+                        <div className="space-y-5 hidden md:block">
+                            <p>Cool. Crisp. Classic</p>
+                            <p className="subtitle">Sip the spirit <br/>of summer</p>
+                        </div>
+                        <div className="view-cocktails">
+                            <p className="subtitle">Every cocktail in our menu is a blend of premium ingredients, creative flair, and timeless recipes - designed to delight your senses</p>
+                            <a href="#cocktails">View Cocktails</a>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <div className="video absolute inset-0">
+                <video
+                    ref={videoRef}
+                    src="/videos/input.mp4"
+                    muted
+                    playsInline={true}
+                    preload={'auto'}></video>
+            </div>
+        </>
+    )
+}
+export default Hero
